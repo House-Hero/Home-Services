a1c951c4531e7cc0e6d0b6a178a9e4286ab15e24
fatma-2117 <fatma.ahmed2202@gmail.com>
2024-11-01 21:03:40 +0200

    add some validation to the customer controller (try and cach )

--- HouseHero/Controllers/CustomerController.cs
+++ HouseHero/Controllers/CustomerController.cs
@@ -3,6 +3,7 @@ using BLL.Interface;
 using BLL.Repository;
 using DAL.Data.Context;
 using DAL.Models;
+using HouseHero.Models;
 using HouseHero.Models.ViewModels;
 using HouseHero.Models.ViewModels.Customer;
 using Microsoft.AspNetCore.Authorization;
@@ -20,59 +21,144 @@ namespace HouseHero.Controllers
         private readonly IServiceRepository _serviceRepository;
         private readonly ICustomerRepository _customerRepository;
         private readonly IRequestRepository _requestRepository;
-       
+        private readonly ICityRepository _cityRepository;
+        private readonly ICloudinaryService _cloudinary;
 
-        public CustomerController( IServiceRepository serviceRepository ,ICustomerRepository customerRepository
-                               , IRequestRepository requestRepository)
+        public CustomerController(IServiceRepository serviceRepository, ICustomerRepository customerRepository,
+                                  IRequestRepository requestRepository, ICityRepository cityRepository,
+                                  ICloudinaryService cloudinary)
         {
             _serviceRepository = serviceRepository;
             _customerRepository = customerRepository;
             _requestRepository = requestRepository;
+            _cityRepository = cityRepository;
+            _cloudinary = cloudinary;
         }
+
         [HttpGet]
         public IActionResult CustomerProfile()
         {
-            // Fetch customer data
-            var applicationUserId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
-            var applicationUser = _customerRepository.GetApplicationUserByApplicationUserId(applicationUserId);
-            var customer = _customerRepository.GetCustomerByApplicationUserId(applicationUser.Id);
+            try
+            {
+                var applicationUserId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                var applicationUser = _customerRepository.GetApplicationUserByApplicationUserId(applicationUserId);
+                var customer = _customerRepository.GetCustomerByApplicationUserId(applicationUser.Id);
 
-            // Populate status dropdown
-            ViewBag.StatusList = new SelectList(
-                Enum.GetValues(typeof(Status))
-                .Cast<Status>()
-                .Select(s => new { Value = (int)s, Text = s.ToString() }),
-                "Value", "Text");
+                ViewBag.StatusList = new SelectList(
+                    Enum.GetValues(typeof(Status))
+                    .Cast<Status>()
+                    .Select(s => new { Value = (int)s, Text = s.ToString() }),
+                    "Value", "Text");
 
-            // Populate services dropdown
-            ViewBag.ServiceList = new SelectList(_serviceRepository.GetAll(), "Id", "Name");
+                ViewBag.ServiceList = new SelectList(_serviceRepository.GetAll(), "Id", "Name");
 
-            return View(customer);
+                return View(customer);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                var req = HttpContext.TraceIdentifier;
+                var errorModel = new ErrorViewModel { RequestId = req };
+                return View("Error", errorModel);
+            }
         }
 
         [HttpPost]
-        public IActionResult FilterRequests(int customerId, int? selectedStatus, int? selectedService, int page = 1, int pageSize = 6)
+        public IActionResult FilterRequests(int customerId, int? selectedStatus, int? selectedService, int page = 1, int pageSize = 8)
         {
-            var filteredRequests = _requestRepository.GetFilterRequestsForCustomer(customerId, selectedStatus, selectedService)
-                                                  .Skip((page - 1) * pageSize)
-                                                  .Take(pageSize)
-                                                  .ToList();
-            var totalRequests = _requestRepository.GetFilterRequestsForCustomer(customerId, selectedStatus, selectedService).Count();
-            var totalPages = (int)Math.Ceiling((double)totalRequests / pageSize);
+            try
+            {
+                var filteredRequests = _requestRepository.GetFilterRequestsForCustomer(customerId, selectedStatus, selectedService)
+                                                    .Skip((page - 1) * pageSize)
+                                                    .Take(pageSize)
+                                                    .ToList();
+                var totalRequests = _requestRepository.GetFilterRequestsForCustomer(customerId, selectedStatus, selectedService).Count();
+                var totalPages = (int)Math.Ceiling((double)totalRequests / pageSize);
 
-            return Json(new { requests = filteredRequests, totalPages });
+                return Json(new { requests = filteredRequests, totalPages });
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                var req = HttpContext.TraceIdentifier;
+                var errorModel = new ErrorViewModel { RequestId = req };
+                return View("Error", errorModel);
+            }
         }
 
         [HttpPost]
         public IActionResult CancelRequest(int requestId)
         {
-            var request = _requestRepository.Get(requestId);
-            if (request != null && request.Status == Status.on_Review)
+            try
             {
-                _requestRepository.Delete(request);
-                return Ok();
+                var request = _requestRepository.Get(requestId);
+                if (request != null && request.Status == Status.on_Review)
+                {
+                    _requestRepository.Delete(request);
+                    return Ok();
+                }
+                return BadRequest();
             }
-            return BadRequest();
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                var req = HttpContext.TraceIdentifier;
+                var errorModel = new ErrorViewModel { RequestId = req };
+                return View("Error", errorModel);
+            }
+        }
+
+        public IActionResult Edit(int id)
+        {
+            try
+            {
+                Customer customer = _customerRepository.GetAllCustomerDetiles(id);
+                EditCustomerViewModel viewModel = customer;
+                TempData["ImageUrl"] = viewModel.ImageUrl;
+                viewModel.Cities = _cityRepository.GetAll().ToList();
+                return View(viewModel);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                var req = HttpContext.TraceIdentifier;
+                var errorModel = new ErrorViewModel { RequestId = req };
+                return View("Error", errorModel);
+            }
+        }
+
+        public async Task<IActionResult> SaveChange(int id, EditCustomerViewModel Edit)
+        {
+            try
+            {
+                Edit.CustomerId = id;
+                Edit.ImageUrl = TempData["ImageUrl"] != null ? TempData["ImageUrl"].ToString() : null;
+                Customer customer = Edit;
+                ApplicationUser user = Edit;
+
+                if (Edit.ImageUrl == null && Edit.Image != null)
+                    user.ProfilePicture_ID = await _cloudinary.UploadImageAsync(Edit.Image);
+                else if (Edit.ImageUrl != null && Edit.Image != null)
+                {
+                    string url = Edit.ImageUrl;
+                    string pattern = @"\/([^\/]+)\.jpg$";
+                    Match match = Regex.Match(url, pattern);
+                    if (match.Success)
+                    {
+                        string ProfileID = match.Groups[1].Value;
+                        user.ProfilePicture_ID = await _cloudinary.UpdateImageAsync(ProfileID, Edit.Image);
+                    }
+                }
+                _customerRepository.UpdateCustomerApplactionUser(user);
+                return RedirectToAction("GetAll", "Category");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+                var req = HttpContext.TraceIdentifier;
+                var errorModel = new ErrorViewModel { RequestId = req };
+                return View("Error", errorModel);
+            }
         }
     }
 }
